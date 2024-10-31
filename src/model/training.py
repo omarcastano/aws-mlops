8d28f35b2b157a7be5d853827b41b801ac128054
Omar Andres Castaño <omar.castano@udea.edu.co>
2024-10-31 17:09:41 -0500

    update training module

---
+++ src/model/training.py
@@ -0,0 +1,17 @@
+from src.model.model import TitanicModel
+from src.etl.load_data import load_data_from_s3
+import boto3
+
+# load data
+X_train, X_test, y_train, y_test = load_data_from_s3("omar-mlops-datasets", "titanic.csv")
+
+# train model
+model = TitanicModel()
+model.fit(X_train, y_train)
+
+## upload model to s3
+bucket_name = "trained-models-omar"
+model_name = "titanic_model.pkl"
+
+# upload pickled model to s3
+model.save_model_in_s3(bucket_name, model_name)
