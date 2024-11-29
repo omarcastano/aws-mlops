12a360b35defa105a408215f610a81bd0150d626
Omar Andres Castaño <omar.castano@udea.edu.co>
2024-11-29 17:25:58 -0500

    updated install_dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # List the current directory contents for debugging
-echo "Current Directory:"
-pwd
-ls
-sudo mkdir -p /home/ec2-user/ml-project
-cd /home/ec2-user/ml-project/
-echo "Current Directory:"
-pwd
-ls
-# curl -sSL https://install.python-poetry.org | python
-# export PATH=$HOME/.local/bin:$PATH
-# poetry shell
-# poetry install
+# echo "Current Directory:"
+# pwd
+# ls
+# sudo mkdir -p /home/ec2-user/ml-project
+# cd /home/ec2-user/ml-project/
+# echo "Current Directory:"
+# pwd
+# ls
+cd /home/user/ml-app
+curl -sSL https://install.python-poetry.org | python
+export PATH=$HOME/.local/bin:$PATH
+poetry shell
+poetry install
