85b62453367c9d2071f179dc720cbfc1801894e4
Omar Andres Castaño <omar.castano@udea.edu.co>
2024-12-02 16:06:41 -0500

    updated install_dependencies

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,5 +2,6 @@
 curl -sSL https://install.python-poetry.org | python
 export PATH=$HOME/.local/bin:$PATH
 poetry shell
-poetry install /home/user/ml-app/pyproject.toml
+cd cd /home/user/ml-app
+poetry install
 python /home/user/ml-app/src/app/app.py
