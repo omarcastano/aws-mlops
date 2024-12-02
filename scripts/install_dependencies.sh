6945d3649aea4999aa6ec7f984442156a3e142c7
Omar Andres Castaño <omar.castano@udea.edu.co>
2024-12-02 16:17:21 -0500

    updated install_dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -sSL https://install.python-poetry.org | python
+curl -sSL https://install.python-poetry.org | sudo python
 echo "export PATH=$HOME/.local/bin:$PATH" >> /root/.bashrc
