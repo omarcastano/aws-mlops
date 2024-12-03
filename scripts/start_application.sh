fd6e487d2811143d0c7e0933d6fcad3242e39185
Omar Andres Castaño <omar.castano@udea.edu.co>
2024-12-03 10:48:41 -0500

    updated install_dependencies

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 cd /home/user/ml-app
-# Source user's profile to get correct environment
-source /home/user/.profile
-source /home/user/.bashrc
 ls
 pwd
-python3.10 --version
-curl -sSL https://install.python-poetry.org | python3.10
+/usr/bin/python3.10 --version
+curl -sSL https://install.python-poetry.org | /usr/bin/python3.10
 export PATH=$HOME/.local/bin:$PATH
 poetry shell
 poetry install
-poetry run python3.10 /src/app/app.py
+poetry run /usr/bin/python3.10 /src/app/app.py
