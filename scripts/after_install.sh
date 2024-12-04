5bed26c17edec90cdb7ac4877ab98bf515e930a6
Omar Andres Castaño <omar.castano@udea.edu.co>
2024-12-04 11:22:55 -0500

    updated install_dependencies

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -ex
-CD_INSTALL_TARGET=/home/omar/ml-app
+# set -ex
+# CD_INSTALL_TARGET=/home/omar/ml-app
 
-echo "copying systemd unit file in place"
-sudo cp "${CD_INSTALL_TARGET}/configs/python-app.service" /etc/systemd/system/python-app.service
-sudo systemctl daemon-reload
+# echo "copying systemd unit file in place"
+# sudo cp "${CD_INSTALL_TARGET}/configs/python-app.service" /etc/systemd/system/python-app.service
+# sudo systemctl daemon-reload
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# cd /home/omar/ml-app
+# curl -sSL https://install.python-poetry.org | python
+# export PATH=$HOME/.local/bin:$PATH
+# poetry shell
+# poetry install
+# sudo systemctl enable python-app
+# sudo systemctl start python-app
 cd /home/omar/ml-app
-curl -sSL https://install.python-poetry.org | python
-export PATH=$HOME/.local/bin:$PATH
-poetry shell
-poetry install
-sudo systemctl enable python-app
-sudo systemctl start python-app
+sudo docker compose build
+sudo docker compose up -d
